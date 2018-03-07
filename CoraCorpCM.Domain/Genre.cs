7a416b203bd651148d5dd6fa2e3238e27df4d7ab
Andrew Boza <aboza@meridianassetservices.com>
2018-03-07 14:56:43 -0500

    Adds ArtistGenre to Genre

--- CoraCorpCM.Domain/Genre.cs
+++ CoraCorpCM.Domain/Genre.cs
@@ -1,12 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoraCorpCM.Domain
 {
     public class Genre
     {
+        public Genre()
+        {
+            ArtistGenres = new HashSet<ArtistGenre>();
+        }
+
         public int Id { get; set; }
         public Museum Museum { get; set; }
         [Required]
         public string Name { get; set; }
+        public ICollection<ArtistGenre> ArtistGenres { get; set; }
     }
 }
