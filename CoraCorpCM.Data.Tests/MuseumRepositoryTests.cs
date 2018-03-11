09a8bc062021a055024122310b943d2a16793255
Andrew Boza <andrew.m.boza@gmail.com>
2018-03-11 15:47:39 -0400

    Adds Data layer test project and first inMemory EF test

--- CoraCorpCM.Data.Tests/MuseumRepositoryTests.cs
+++ CoraCorpCM.Data.Tests/MuseumRepositoryTests.cs
@@ -1,4 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using CoraCorpCM.Domain.Models;
+using System.Linq;
 
 namespace CoraCorpCM.Data.Tests
 {
@@ -6,8 +10,42 @@ namespace CoraCorpCM.Data.Tests
     public class MuseumRepositoryTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Insert_WithNewMuseum_WritesToTheDatabase()
         {
+            // In-memory database only exists while the connection is open
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                // Create the schema in the database
+                using (var context = new ApplicationDbContext(options))
+                {
+                    context.Database.EnsureCreated();
+                }
+
+                // Run the test against one instance of the context
+                using (var context = new ApplicationDbContext(options))
+                {
+                    var repo = new MuseumRepository(context);
+                    repo.Insert(new Museum { Name = "Test Museum", ShortName = "TM" });
+                }
+
+                // Use a separate instance of the context to verify correct data was saved to database
+                using (var context = new ApplicationDbContext(options))
+                {
+                    Assert.AreEqual(1, context.Museums.Count());
+                    Assert.AreEqual("Test Museum", context.Museums.Single().Name);
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
